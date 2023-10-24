301a1986da50b76c6946256801a1b1c0aca2f1cc
RiskeMan <dhzm987@naver.com>
2023-10-24 11:29:42 +0900

    docmall

---
+++ src/main/java/com/docmall/controller/BoardController.java
@@ -0,0 +1,15 @@
+package com.docmall.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Controller
+public class BoardController {
+
+	public void a() {
+		log.info("dkdkdkdkdk");
+	}
+}
